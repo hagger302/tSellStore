6b14e9d4f265d2372d103d90dcd8c99eaccfe734
DELL <tolemyhagger3@gmail.com>
2021-03-29 00:28:53 +0600

    DashbordAdapter updated and error gone...

--- app/src/main/java/com/example/tsellstore/NavigationComponent/Dashbord/MainRecyclerView/DashbordAdapter.java
+++ app/src/main/java/com/example/tsellstore/NavigationComponent/Dashbord/MainRecyclerView/DashbordAdapter.java
@@ -1,21 +1,26 @@
 package com.example.tsellstore.NavigationComponent.Dashbord.MainRecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.tsellstore.NavigationComponent.Dashbord.ViewPager.SliderAdapter;
+import com.example.tsellstore.NavigationComponent.Dashbord.ViewPager.SliderModel;
 import com.example.tsellstore.R;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DashbordAdapter extends RecyclerView.Adapter {
 
-    public DashbordModel dashbordModel;
-
-
+    //////////////////////////--------BANNER sLIDER vIEWpAGGER---------->>>>>>>>>>>>>>>>>>>>>
     private List<DashbordModel> dashbordModelList;
 
     public DashbordAdapter(List<DashbordModel> dashbordModelList) {
@@ -31,7 +36,7 @@ public class DashbordAdapter extends RecyclerView.Adapter {
              * viewType diye LayoutInflater r maddhome BANNER_SLIDER r layout inflate korbo.
              */
             case 0:
-                return dashbordModel.BANNER_SLIDER;
+                return DashbordModel.BANNER_SLIDER;
             default:
                 return -1;
         }
@@ -42,32 +47,142 @@ public class DashbordAdapter extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         switch (viewType) {
-            case dashbordModel.BANNER_SLIDER:
+            /**
+             *  BannerSliderViewHolder r maddhome view ta return korbo.
+             */
+            case DashbordModel.BANNER_SLIDER:
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.banner_slider_layout, parent, false);
                 return new BannerSliderViewHolder(view);
             default:
                 return null;
         }
-        /**
-         *  BannerSliderViewHolder r maddhome view ta return korbo.
-         */
-        return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        /**
+         * Data Binding korbo Banner Slider e
+         */
+        switch (dashbordModelList.get(position).getType()) {
+            case DashbordModel.BANNER_SLIDER:
+                /**
+                 * Banner Slider e joto gula list ase sob gula access korbo tai list banabo
+                 * list ta hobe slider model r thn list ber kora r por amra viewHolder e bosabo
+                 * onk gula view niye kaj korbo tw oi jonno specicly bole dibo kon iew te bosbe
+                 * orthat viewholder custing korbo
+                 */
+                List<SliderModel> sliderModelList = dashbordModelList.get(position).getSliderModelList();
 
+                ((BannerSliderViewHolder) holder).setBannerSliderViewPagger(sliderModelList);
+                break;
+            default:
+                return;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dashbordModelList.size();
     }
 
     public class BannerSliderViewHolder extends RecyclerView.ViewHolder {
 
+        //////////////////////////--------BANNER sLIDER vIEWpAGGER---------->>>>>>>>>>>>>>>>>>>>>
+        private ViewPager bannerSliderViewPagger;
+        private int currentPage = 2;
+        private Timer timer;
+        final private long DELAY_TIME = 3000;
+        final private long PERIOD_TIME = 3000;
+
         public BannerSliderViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            bannerSliderViewPagger = (ViewPager) itemView.findViewById(R.id.banner_slider_viewPager);
+
+        }
+
+        //////////////////////////--------BANNER sLIDER vIEWpAGGER---------->>>>>>>>>>>>>>>>>>>>>
+
+        private void setBannerSliderViewPagger(List<SliderModel> sliderModelList) {
+            SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+            bannerSliderViewPagger.setAdapter(sliderAdapter);
+
+            bannerSliderViewPagger.setClipToPadding(false);
+            bannerSliderViewPagger.setPageMargin(20);
+            bannerSliderViewPagger.setCurrentItem(currentPage);
+
+            ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+                    if (state == ViewPager.SCROLL_STATE_IDLE) { //idle dewa r katron holo amader page r animationb ss and page second page e shift hoye gese so, eta user r samne dhora porbe na
+                        PagerLooper(sliderModelList);
+                    }
+
+                }
+            };
+
+            bannerSliderViewPagger.addOnPageChangeListener(onPageChangeListener);
+
+            startBannerSlideShow(sliderModelList);
+            //user jkn touch kore dhore rakbe tkn bondho hbe slide show --> facebook story type
+            bannerSliderViewPagger.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    PagerLooper(sliderModelList);
+                    stopBannerSlideShow();
+
+                    if (event.getAction() == MotionEvent.ACTION_UP) { //ACTION_UP mane holo user jkn hat sorai nibe
+                        startBannerSlideShow(sliderModelList);
+                    }
+                    return false;
+                }
+            });
+        }
+
+        private void PagerLooper(List<SliderModel> sliderModelList) {
+            //1 by 1 banner chage hbe
+
+            if (currentPage == sliderModelList.size() - 2) {
+                currentPage = 2;
+                bannerSliderViewPagger.setCurrentItem(currentPage, false);
+            }
+            if (currentPage == 1) {
+                currentPage = sliderModelList.size() - 3;
+                bannerSliderViewPagger.setCurrentItem(currentPage, false);
+            }
+        }
+
+        private void startBannerSlideShow(List<SliderModel> sliderModelList) {
+            Handler handler = new Handler();
+            Runnable update = new Runnable() {
+                @Override
+                public void run() {
+                    if (currentPage >= sliderModelList.size()) {
+                        currentPage = 1;
+                    }
+                    bannerSliderViewPagger.setCurrentItem(currentPage++, true); //animation true
+                }
+            };
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(update);
+                }
+            }, DELAY_TIME, PERIOD_TIME);
+        }
+
+        private void stopBannerSlideShow() {
+            timer.cancel();
         }
     }
 }
